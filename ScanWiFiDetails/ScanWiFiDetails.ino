063b469b9d52ad74436857d702804c9373b22ffa
U-Tre <42976531+U-Tre@users.noreply.github.com>
2018-09-08 20:28:54 +0700

    Add files via upload

---
+++ ScanWiFiDetails/ScanWiFiDetails.ino
@@ -0,0 +1,86 @@
+#include "ESP8266WiFi.h"
+
+void setup() {
+  Serial.begin(115200);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
+
+  Serial.println("Setup done");
+
+}
+
+void loop() {
+  Serial.println("scan start");
+
+  // Print WiFi MAC address:
+  printMacAddress();
+ 
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0)
+    Serial.println("no networks found");
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+              
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(" dBm)");
+
+      if(WiFi.RSSI(i) >= -55)
+      {
+        Serial.print("  meter");
+      }else if(WiFi.RSSI(i) < -55 && WiFi.RSSI(i) >= -65)
+      {
+        Serial.print(" 4 meter");
+      }else if(WiFi.RSSI(i) < -65 && WiFi.RSSI(i) >= -75)
+      {
+        Serial.print(" 6 meter");
+      }else if(WiFi.RSSI(i) < -75 && WiFi.RSSI(i) >= -85)
+      {
+        Serial.print(" 8 meter");
+      }else
+      {
+        Serial.print(" 12 meter");
+      }
+      Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*"); 
+      delay(10);
+    }
+    Serial.println("=========================");
+  }
+  Serial.println("");
+
+  // Wait a bit before scanning again
+  delay(10000);
+}
+
+void printMacAddress() {
+  // the MAC address of your Wifi shield
+  byte mac[6];
+
+  // print your MAC address:
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+}
---
+++ ultra/ultra.ino
@@ -0,0 +1,68 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+
+#define trigPin D1
+#define echoPin D2
+int ledPin = D5;
+
+const char *ssid = "Device1";
+const char *password = "";
+
+ESP8266WebServer server(80);
+
+void handleRoot() {
+  server.send(200, "text/html", "<h1>You are connected</h1>");
+}
+
+void setup() {
+  delay(1000);
+  Serial.begin (115200);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  
+  WiFi.softAP(ssid, password);
+
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  float duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+ 
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  distance = ((duration / 2) * 0.0344) / 100;
+  
+  if (distance >= 2.5 || distance <= 0.02){
+    Serial.print("Distance = ");
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print("Distance = ");
+    Serial.print(distance);
+    Serial.println(" m. ");
+
+    if(distance <= 1){
+      digitalWrite(ledPin, HIGH);
+    }else{
+      digitalWrite(ledPin, LOW);
+    }
+  }
+  delay(3000);
+  server.handleClient();
+}
+
